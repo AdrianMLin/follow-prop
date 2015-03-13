67f4120798e9b5b0a26d57f83ce864dacc995484
AdrianMLin <adrian.mx.lin@gmail.com>
2015-03-12 23:31:12 -0400

    stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,21 @@
+
 class HomeController < ApplicationController
 
   def index
+
+    pop_full = HTTParty.get('https://api.instagram.com/v1/media/popular?access_token=23131423.223b0db.6f8d56ddbfce4128936ecf2e8e926dce')['data']
+    
+    pop_image_ids = []
+
+
+    # get all popular posts
+
+    # get ids
+
+    # from ids get all likes
+
     binding.pry
+
     render(:index)
   end
 end
