9d3b31388f154396b7da9856731dbe3511dc74ad
AdrianMLin <adrian.mx.lin@gmail.com>
2015-03-14 15:43:29 -0400

    api calls now work, sometimes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,24 +3,53 @@ class HomeController < ApplicationController
 
   def index
 
-    pop_full = HTTParty.get('https://api.instagram.com/v1/media/popular?access_token=23131423.223b0db.6f8d56ddbfce4128936ecf2e8e926dce')['data']
+    pop = HTTParty.get('https://api.instagram.com/v1/media/popular?access_token=23131423.223b0db.6f8d56ddbfce4128936ecf2e8e926dce')['data']
     
     pop_image_ids = []
 
 
-    # get all popular posts
+    commenters = []
+    likers = []
 
-    # get ids of images
+    pop.each do |item|
+      pop_image_ids << item["id"] # get ids of images
 
-    # from  image ids get all likes
 
-    # from likes get all users
 
-    # paginate users, sort by ratio
+      item['comments']['data'].each do |commenter| # get all commenters
+        user = commenter['from'] # get user
+        user_data = HTTParty.get("https://api.instagram.com/v1/users/#{ user['id'] }/?access_token=23131423.223b0db.6f8d56ddbfce4128936ecf2e8e926dce")
 
-    # send users to models
+        if user_data["data"] #if not throttled
+          counts = user_data["data"]["counts"] #get relevant data
+          user["counts"] = counts #input relevant data into user hash
+        end 
+
+        commenters << user
+      end
+     
+      
+      # item['likes']['data'].each do |liker| # get all likers
+      #   user = liker["from"]
+
+      #   user_id = user['id'] # get user_id to make api call for user
+      #   user_data = HTTParty.get("https://api.instagram.com/v1/users/#{ user['id'] }/?access_token=23131423.223b0db.6f8d56ddbfce4128936ecf2e8e926dce")
+
+      #   if user_data["data"]
+      #     counts = user_data["data"]["counts"] #get relevant data
+      #     user["counts"] = counts #input relevant data into user hash
+      #   end
+
+      #   likers << user
+      # end
+      
+    end
 
     binding.pry
+
+
+    # now check follower count
+
 
     render(:index)
   end
--- client_id.rb
+++ client_id.rb
@@ -7,7 +7,15 @@ code = '63ae3e29f62a47048f0622ce650bf2f7'
 a = '63ae3e29f62a47048f0622ce650bf2f7&gws_rd=ssl'
 
 access_token = '23131423.223b0db.6f8d56ddbfce4128936ecf2e8e926dce'
+
+
+### popular media
+# HTTParty.get('https://api.instagram.com/v1/media/popular?access_token=23131423.223b0db.6f8d56ddbfce4128936ecf2e8e926dce')
+
+
 # curl -F 'client_id=223b0db9bbf24e75bbf7ea6b6f6ef5b1' -F 'client_secret=3062c10edcc24fd88c78129ef7718233' -F 'grant_type=authorization_code' -F 'redirect_uri=http://www.google.com' -F 'code=63ae3e29f62a47048f0622ce650bf2f7' https://api.instagram.com/oauth/access_token
 
 
 # {"access_token":"23131423.223b0db.6f8d56ddbfce4128936ecf2e8e926dce","user":{"username":"adrianmlin","bio":"Linguist, web developer, photographer. I play with composition and lighting. All photos by me or of me.","website":"http:\/\/AdrianLin.com","profile_picture":"https:\/\/instagramimages-a.akamaihd.net\/profiles\/profile_23131423_75sq_1389942643.jpg","full_name":"Adrian M Lin","id":"23131423"}}
+
+
