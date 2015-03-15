120134e0e3c320e2277f467aa1bddfd5e6238729
AdrianMLin <adrian.mx.lin@gmail.com>
2015-03-15 17:38:04 -0400

    data shows up in erb now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -25,11 +25,14 @@ class HomeController < ApplicationController
           counts = user_data["data"]["counts"] #get relevant data
           user["counts"] = counts #input relevant data into user hash
 
-          num_followed_by = counts["followed_by"]
-          num_follows = counts["follows"]
+          num_followed_by = user['counts']["followed_by"]
+          num_follows = user['counts']["follows"]
 
           num_followed_by = 0.01 if num_followed_by <= 0 #can't divide by zero
           user["following_to_follower_ratio"] = num_follows / num_followed_by.to_f #need to make it a fraction. integer / integer must return integer
+        else
+          user["counts"] = {media: 0, followed_by: 0, follows: 0}
+          user['following_to_follower_ratio'] = 0
         end 
 
 
