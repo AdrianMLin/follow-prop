299c4717ba2411e485fed3f2098b2d59750daf66
AdrianMLin <adrian.mx.lin@gmail.com>
2015-03-20 22:23:47 -0400

    stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,16 @@
 class HomeController < ApplicationController
 
   def index
-
     pop = HTTParty.get('https://api.instagram.com/v1/media/popular?access_token=23131423.223b0db.6f8d56ddbfce4128936ecf2e8e926dce')['data']
 
-
-    pop_image_ids = []
+    # pop_image_ids = []
 
 
     commenters = []
-    likers = []
+    # likers = []
 
     pop.each do |item|
-      pop_image_ids << item["id"] # get ids of images
+      # pop_image_ids << item["id"] # get ids of images
 
       item['comments']['data'].each do |commenter| # get all commenters
 
@@ -27,14 +25,14 @@ class HomeController < ApplicationController
           num_followed_by = user['counts']["followed_by"]
           num_follows = user['counts']["follows"]
 
-          num_followed_by = 0.01 if num_followed_by <= 0 #can't divide by zero
-          user["following_to_follower_ratio"] = num_follows / num_followed_by.to_f #need to make it a fraction. integer / integer must return integer
+          num_followed_by = 0.01 if num_follows <= 0 #can't divide by zero
+          user["followed_by_to_follows_ratio"] = num_followed_by / num_follows.to_f #need to make it a fraction. integer / integer must return integer
         else
           user["counts"] = {media: 0, followed_by: 0, follows: 0}
-          user['following_to_follower_ratio'] = 0
+          user['followed_by_to_follows_ratio'] = 0
         end 
 
-        commenters << user unless user['following_to_follower_ratio'] == 0
+        commenters << user unless user['followed_by_to_follows_ratio'] == 0
       end
      
       # item['likes']['data'].each do |liker| # get all likers
@@ -52,7 +50,10 @@ class HomeController < ApplicationController
       # end
       
     end
-    commenters.sort_by { |hash| hash["following_to_follower_ratio"] } #sort by ratio
+
+
+
+    commenters.sort_by { |hash| hash["followed_by_to_follows_ratio"] } #sort by ratio
 
     binding.pry
 
